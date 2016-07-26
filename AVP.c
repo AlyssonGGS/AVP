ea44ed57ea6b68b4eb7abf22fd4029fed86d3459
AlyssonGGS <alyssonggs@hotmail.com>
2016-07-25 23:53:24 -0300

    Remoção

--- AVP.c
+++ AVP.c
@@ -549,7 +549,7 @@ int main(void)
     char nome[30];
     TAluno *a = NULL;
     int c = 0,maiorMat = 0;
-    while(c < 100){
+    while(c < 4){
         //pega matrícula
         fscanf(fp, "%s", buff);
         mat = atoi(buff);
